6ef42b875900a6251d0e9d183807c8dc17505fc8
cfx <chenfuxian2015@163.com>
2022-01-18 23:40:14 +0800

    add 12322

--- src/test/java/com/cfx/MessageTest.java
+++ src/test/java/com/cfx/MessageTest.java
@@ -3,6 +3,7 @@ package com.cfx;
 import com.cfx.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
 /**
@@ -10,7 +11,7 @@ import org.testng.annotations.Test;
  * @Date: 2022/1/15 11:22
  */
 @SpringBootTest(classes = Application.class)
-public class MessageTest {
+public class MessageTest extends AbstractTestNGSpringContextTests {
 
     @Autowired
     private EmailService emailService;
